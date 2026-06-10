b78c159ef873f3081d30b2c9d8785c5fe92daa75
Pedro <pedroluchese@gmail.com>
2026-06-09 22:25:10 -0300

    layout arrumado

--- app/src/main/java/com/example/rumo/Rumo.java
+++ app/src/main/java/com/example/rumo/Rumo.java
@@ -1,11 +1,15 @@
 package com.example.rumo;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -18,7 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Rumo extends AppCompatActivity {
-
+    private Intent it;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -80,5 +84,13 @@ public class Rumo extends AppCompatActivity {
         // Configura o RecyclerView vertical
         rvCurriculos.setLayoutManager(new LinearLayoutManager(this));
         rvCurriculos.setAdapter(new CurriculoAdapter(this, itens));
+    }
+    public void telaCurriculo(View view){
+        it = new Intent(getApplicationContext(), EscolhaCurriculo.class);
+        startActivity(it);
+    }
+    public void telaManutencao(View view){
+        it = new Intent(getApplicationContext(), ManutencaoUsuario.class);
+        startActivity(it);
     }
 }
